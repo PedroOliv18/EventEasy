12b86a20d97b4d4e9f934912a2dd98053115b737
Pedro Henrique Oliveira <pedrohenriqueo2016@outlook.com>
2020-06-05 15:21:08 -0300

    Textinho bonitinho

--- app/src/main/java/com/example/eventeasy/Splash.java
+++ app/src/main/java/com/example/eventeasy/Splash.java
@@ -9,7 +9,7 @@ import android.os.Handler;
 public class Splash extends AppCompatActivity {
 
     // Tempo da splash screen
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
